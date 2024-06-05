f49fd77b2cde0800add1fd42b6f800d9beab4509
lucabro81 <luca.brognara81@gmail.com>
2024-06-05 16:07:47 +0200

    feat: first commit

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,68 @@
+import path, { join, dirname } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+  stories: ["../../../packages/components/ui/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-actions"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-designs"),
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [
+          path.resolve(
+            path.dirname(__dirname),
+            "../..",
+            "packages/components",
+            "tsconfig.json"
+          ),
+        ],
+      })
+    );
+
+    return {
+      ...config,
+      build: {
+        ...config.build,
+        rollupOptions: {
+          ...config.build?.rollupOptions,
+          // this is required to avoid not necessary warnings
+          onwarn(warning, warn) {
+            if (
+              ["MODULE_LEVEL_DIRECTIVE", "SOURCEMAP_ERROR"].includes(
+                warning.code ?? ""
+              )
+            ) {
+              return;
+            }
+            warn(warning);
+          },
+        },
+      },
+    };
+  },
+};
+export default config;
---
+++ packages/components/index.ts
@@ -0,0 +1,4 @@
+import Card from "./ui/card.vue";
+import Gradient from "./ui/gradient.vue";
+
+export { Card, Gradient };
